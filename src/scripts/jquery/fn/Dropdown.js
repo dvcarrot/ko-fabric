22c4570122e1978067f5535d4b88c94dc9fa553d
dvcarrot <dvcarrot@yandex.ru>
2017-09-04 17:19:17 +0300

    jquery Dropdown widget

---
+++ src/scripts/jquery/fn/Dropdown.js
@@ -0,0 +1,28 @@
+(function ($, fabric) {
+
+    $.fn.Dropdown = function () {
+
+        return this.each(function () {
+
+            var self = this;
+            var $self = $(self);
+
+            function refresh() {
+
+                $self.find('.ms-Dropdown-title, .ms-Dropdown-items').remove();
+
+                if ($self.find('option').length > 0) {
+                    new fabric['Dropdown'](self);
+                }
+
+            }
+
+            refresh();
+
+            $(this).on('refresh', refresh);
+
+        });
+
+    };
+
+})(jQuery, fabric);
